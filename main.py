426cb6df65ea2880cbdc8fcbf768a64e6337cdfd
JavaUroz <urozjavier@gmail.com>
2022-10-31 13:13:31 -0300

    version2

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+from importlib.resources import path
 import streamlit as st
 import pandas as pd
-from modules.functions import agrega_precios
+from sqlalchemy import create_engine
+from modules.functions import agrega_tablas
 from modules.functions import define_interfaz
-# from sqlalchemy import create_engine
 
 # Definición de los controles de la barra lateral
 level_user = st.sidebar.selectbox(label='Nivel de usuario', options=['Novato', 'Experto'])
 
 # Conexión con SQLAlchemy
-# path = '.\data/'
-# database = 'DSS.db'
-# my_conn=create_engine('sqlite:///'+path+database)
+path = './data/'
+database = 'DSS-Autos.db'
+my_conn=create_engine('sqlite:///'+path+database)
 
 # Carga los datos de la aplicación
-data_vehiculos = pd.read_csv('./data/vehiculos.csv')
-data_precios = pd.read_csv('./data/precios.csv', dtype={'Precio': float})
-# data_vehiculos = pd.read_sql_table('autos', my_conn)
-# data_precios = pd.read_sql_table('precios', my_conn)
-# data_criterios = pd.read_sql_table('criterios', my_conn)
+vehiculos = pd.read_sql_table('autos',my_conn)
+precios = pd.read_sql_table('precios',my_conn)
+criterios = pd.read_sql_table('criterios', my_conn)
 
 # Fusiona los precios con la base de datos de vehículos.
-data = agrega_precios(data_vehiculos, data_precios)
+data = agrega_tablas(vehiculos, precios)
+
+# Fusiona los precios con la base de datos de vehículos.
+data = agrega_tablas(vehiculos, criterios)
 
 # Definición del panel central
 st.header('Sistema de apoyo para la elección de vehículos')
