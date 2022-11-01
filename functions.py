10af90803d207cfd02b0dbdc07e527b43aa88e85
JavaUroz <urozjavier@gmail.com>
2022-11-01 14:00:52 -0300

    0200

--- functions.py
+++ functions.py
@@ -49,7 +49,7 @@ def interfaz_novato(data):
         tipo = col2.multiselect('Tipo de carrocería', sorted(data['TipoVehiculo'].unique().tolist()))
         if tipo == []:
             col2.error('Elija al menos un tipo de carrocería')
-        precio_max = col1.slider('Precio en miles de pesos', 0, 10000)
+        precio_max = st.slider('Precio en miles de pesos', 0, 10000)
     # Aplica las opciones de filtrado
     filtrado = data[(data['Marca'].isin(marca)) & (data['Precio'] < precio_max)]
     # Aplica la matriz de decisión y la guarda en la variable ponderacion.
