fb81aa0bb0aaeb4056a6adfb53fd816fc73bcd18
JavaUroz <urozjavier@gmail.com>
2022-11-01 08:18:02 -0300

    prueba0817

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import streamlit as st
 import pandas as pd
+from sqlalchemy import create_engine
 from functions import agrega_precios
 from functions import define_interfaz
 
 # Definición de los controles de la barra lateral
 level_user = st.sidebar.selectbox(label='Nivel de usuario', options=['Novato', 'Experto'])
 
+# # Conexión con SQLAlchemy
+path = 'data/'
+database = 'DSS-Autos.db'
+my_conn = create_engine('sqlite:///'+path+database)
+
 # Carga los datos de la aplicación
-vehiculos = pd.read_csv('data/vehiculos.csv')
-precios = pd.read_csv('data/precios.csv', dtype={'Precio': float})
+vehiculos = pd.read_sql_table('autos',my_conn)
+precios = pd.read_sql_table('precios',my_conn)
+criterios = pd.read_sql_table('criterios', my_conn)
+
 # Fusiona los precios con la base de datos de vehículos.
 data = agrega_precios(vehiculos, precios)
 
