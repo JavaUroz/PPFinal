d76ed044cbc2db6c9c4ed2f4c781748ed8e65b67
JavaUroz <urozjavier@gmail.com>
2022-11-02 14:25:58 -0300

    prueba0225

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from functions import agrega_tablas
 from functions import define_interfaz
 
 # Definición de los controles de la barra lateral
+st.header('Sistema de apoyo para la elección de vehículos')
+
 st.header('Conteste estas preguntas para definir su perfil')
 st.subheader('Cual es la unidad de medida de la potencia de un vehiculo?')
 hp = st.checkbox('HP')
