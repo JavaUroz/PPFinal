3f6d02a81a89cc57f09633d0deb98c139bced467
JavaUroz <urozjavier@gmail.com>
2022-11-03 12:23:01 -0300

    v2

--- functions.py
+++ functions.py
@@ -1,9 +1,7 @@
 import streamlit as st
-
 from classes import AutoSuv, Camioneta
 
-def formulario_interfaz():
-      
+def formulario_interfaz():      
     placeholder = st.empty()
     with placeholder.form("form_interfaz"):
         st.title('Sistema de apoyo para la elección de vehículos')
