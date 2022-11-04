a118e879855ea3005c6b6f6e4fabe9bcf7367aa3
JavaUroz <urozjavier@gmail.com>
2022-11-04 08:04:58 -0300

    v2

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from functions import *
 data = conexion_datos()
 
 # Formulario que define la interfaz como parametro
-level_user = formulario_interfaz()
-define_interfaz(level_user, data)
+level_user = None
+if level_user == None:
+    level_user = formulario_interfaz()
+else:
+    define_interfaz(level_user, data)
 
 # if level_user == None:
 #     formulario_interfaz()
